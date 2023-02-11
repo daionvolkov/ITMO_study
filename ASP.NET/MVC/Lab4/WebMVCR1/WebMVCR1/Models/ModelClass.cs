cd54aa65007cfa4bb0860cff6a0b5b5b7d3928a5
daionvolkov <daionvolkov@gmail.com>
2023-02-11 16:26:47 +0300

    commit MVC folder 11.02

---
+++ ASP.NET/MVC/Lab4/WebMVCR1/WebMVCR1/Models/ModelClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace WebMVCR1.Models
+{
+
+
+    public class ModelClass
+    {
+        public static string ModelHello()
+        {
+            int hour = DateTime.Now.Hour;
+            string Greeting = hour < 12 ? "Доброе утро" : "Добрый день"; return Greeting;
+        }
+
+
+       
+
+        
+
+    }
+    
+
+
+}
